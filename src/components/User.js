68082e2021525b327ed40924854005a7c3bee2a6
BERK SAGLICAK <21895855@ogr.baskent.edu.tr>
2022-07-08 01:26:12 +0300

    component denemeleri

---
+++ src/components/User.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class User extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+            <input type="text"/>
+            <button>gönder</button>
+            
+        </form>
+      </div>
+    )
+  }
+}
